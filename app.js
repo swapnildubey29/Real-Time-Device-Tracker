658c50db7f7a4708ac53eb1dad4baa0ecad1c987
swapnildubey29 <swapnildubey3636@gmail.com>
2024-10-11 23:06:13 +0530

    Device Tracker

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const express = require('express')
-const app = express()
-const http = require('http')
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
 
-//Configure
-const socketio = require('socket.io')
-const server = http.createServer(app)
-const io = socketio(server)
+// Configure
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.set("view engine", "ejs")
-app.set(express.static(path.join(__dirname,"public")))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
-app.get("/", function(req,res) {
-     res.send("hey")
+io.on("connection", function(socket) {
+    socket.on("send-location", function(data) { 
+        io.emit("recieve-location", { id: socket.id, ...data });
+    });
+    
+    socket.on("disconnect", function() {
+        io.emit("user-disconnect", socket.id);
+    })
 });
 
-server.listen(8000)
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+server.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
